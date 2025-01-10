907f1b7f5f62406fe53852e183ab308366a93087
Kuba <kosinski.jakub01@gmail.com>
2025-01-10 02:56:26 +0100

    init

---
+++ lib/app/view/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Habit Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Container(),
+    );
+  }
+}
