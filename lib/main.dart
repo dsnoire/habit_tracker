b2fc7775c5406b8f332f32d9062d454278f0becb
Kuba <kosinski.jakub01@gmail.com>
2025-01-12 19:40:21 +0100

    feat: integrate firebase with project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/firebase_options.dart';
 
 import 'app/view/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   final AuthenticationRepository authenticationRepository = AuthenticationRepository();
   runApp(
-    App(
-      authenticationRepository: authenticationRepository,
-    ),
+    App(authenticationRepository: authenticationRepository),
   );
 }
